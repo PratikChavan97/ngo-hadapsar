f58e297ab2049ef8d0ea47998700794b0de43b82
PratikChavan97 <pratikchavan061997@gmail.com>
2025-04-12 00:21:04 +0530

    Build: Routing and Page Not Found

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 import Navigation from "../ui/home/Navigation";
 
 function Home() {
   return (
     <div>
       <Navigation />
-      <Link to="dashboard">Go to Dashboard</Link>
+      <Link to="/dashboard">Go to dashboard</Link>
     </div>
   );
 }
