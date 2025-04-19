c3a11e0c99571a5f0519c361bcdd289df9ad4cf2
PratikChavan97 <pratikchavan061997@gmail.com>
2025-04-19 14:06:23 +0530

    Dashboard Navigation Static Layouting

--- src/Pages/admin/AdminHome.jsx
+++ src/Pages/admin/AdminHome.jsx
@@ -1,10 +1,30 @@
+import { NavLink } from "react-bootstrap";
 import "./module.AdminHome.css";
 
 function AdminHome() {
   return (
-    <div className="fluid-container d-flex">
-      <div className="navigation">Navigation</div>
-      <div className="display">Display</div>
+    <div className="dashboard">
+      <div className="navigation">
+        <h1>"Setting sign" Dashboard</h1>
+        <nav>
+          <NavLink to="">Home</NavLink>
+          <NavLink to="">Donation</NavLink>
+          <NavLink to="">Projects</NavLink>
+          <NavLink to="">Blogs</NavLink>
+          <NavLink to="">Users</NavLink>
+          <NavLink to="">Settings</NavLink>
+          <NavLink to="">Help</NavLink>
+        </nav>
+        <div>
+          <img src="" alt="" />
+          <div>
+            <h3>UserName</h3>
+            <p>User Designation</p>
+          </div>
+          <span>&darr;</span>
+        </div>
+      </div>
+      <div className="data"></div>
     </div>
   );
 }
